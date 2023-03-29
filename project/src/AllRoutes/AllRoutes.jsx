0a0527145ee3bd27a9781844a7ec92e92029f798
Sudip Chowdhury <rexalex826@gmail.com>
2023-03-29 10:12:43 +0530

    pull test

--- project/src/AllRoutes/AllRoutes.jsx
+++ project/src/AllRoutes/AllRoutes.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Route, Routes, }from "react-router-dom"
-import HomePage from '../Pages/HomePage'
-
+// import KidPage from '../pages/KidPage'
+// import HomePage from "../pages/HomePage"
 function AllRoutes() {
   return (
     <div>
         <Routes>
-        <Route path="/" element={<HomePage/>} />
+        {/* <Route path="/" element={<HomePage/>}  ></Route> */}
 
         </Routes>
 
