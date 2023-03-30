c3ae360cff5a61e43f6d4867900d0c640fdb69b4
Sudip Chowdhury <rexalex826@gmail.com>
2023-03-30 07:49:15 +0530

    home page with responsiveness done

--- project/src/component/Navbar.jsx
+++ project/src/component/Navbar.jsx
@@ -4,6 +4,10 @@ import logo from "../photos/logo.png"
 import { BsSearch,BsHandbag,BsQuestionCircle,BsHeart } from "react-icons/bs";
 import {VscAccount} from "react-icons/vsc"
 import { IconContext } from "react-icons";
+import { Link as NavLink } from 'react-router-dom';
+
+
+
 function Navbar() {
   return (
 <>
@@ -12,20 +16,20 @@ function Navbar() {
     </div>
     <div className='bottom-nav'>
         <div className='catagory'>
-        <h4>MAN</h4>
-        <h4>WOMAN</h4>
-        <h4>KID</h4>
+        <NavLink to="/mensection">  <h4>MAN</h4></NavLink> 
+        <NavLink to="/womensection"> <h4>WOMAN</h4></NavLink> 
+        <NavLink to="/kidsection">  <h4>KID</h4></NavLink> 
         <h4>SS'23</h4>
         <h4>LAST CHANCE</h4>
         </div>
-        <img width="18%" src={logo} alt="logo"/>
+       <NavLink to="/"><img  src={logo} alt="logo"/></NavLink>
         <div className='icons'>
             <IconContext.Provider value={{ size:"2em"}}>
           <div>  <BsSearch /></div>
           <div> <VscAccount/></div> 
           <div> <BsQuestionCircle/></div> 
-          <div> <BsHeart/></div> 
-          <div> <BsHandbag/></div> 
+          <NavLink to="/wishlist"> <div> <BsHeart/></div> </NavLink> 
+          <NavLink to="/cart"> <div> <BsHandbag/></div></NavLink> 
           </IconContext.Provider>
         </div>
 
